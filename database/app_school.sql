1cf9c22cee1e6a1aef4763c721815cb72faff2ea
Natã Romão Ferreira <nataromao@pop-os.localdomain>
2021-09-03 15:17:28 -0300

    Adiciona base de dados

---
+++ database/app_school.sql
@@ -0,0 +1,35 @@
+-- --------------------------------------------------------
+-- Servidor:                     localhost
+-- Versão do servidor:           10.3.28-MariaDB-1:10.3.28+maria~focal - mariadb.org binary distribution
+-- OS do Servidor:               debian-linux-gnu
+-- HeidiSQL Versão:              11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para app_school
+CREATE DATABASE IF NOT EXISTS `app_school` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `app_school`;
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+-- Exportação de dados foi desmarcado.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
